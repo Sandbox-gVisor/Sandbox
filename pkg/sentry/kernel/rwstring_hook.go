5d9bb66e8c977c5c070309ffc4679749ea91d91a
Eduard <eduardvartazaryan1@gmail.com>
2023-07-17 12:23:35 +0700

    Renamed functions

--- pkg/sentry/kernel/rwstring_hook.go
+++ pkg/sentry/kernel/rwstring_hook.go
@@ -4,13 +4,13 @@ import (
 	"gvisor.dev/gvisor/pkg/hostarch"
 )
 
-func ReadStringHook(t *Task) func(addr uintptr, len int) (string, error) {
+func ReadStringProvider(t *Task) func(addr uintptr, len int) (string, error) {
 	return func(addr uintptr, length int) (string, error) {
 		return t.CopyInString(hostarch.Addr(addr), length)
 	}
 }
 
-func WriteStringHook(t *Task) func(addr uintptr, str string) (int, error) {
+func WriteStringProvider(t *Task) func(addr uintptr, str string) (int, error) {
 	return func(addr uintptr, str string) (int, error) {
 		bytes := []byte(str)
 		return t.CopyOutBytes(hostarch.Addr(addr), bytes)
