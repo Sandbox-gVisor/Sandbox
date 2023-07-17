92dafa04cb282e56f2d14b985c68ccf21b5a4fd9
Eduard <eduardvartazaryan1@gmail.com>
2023-07-17 11:55:30 +0700

    fix moved hook's package and changed BUILD

--- pkg/sentry/kernel/rwstring_hook.go
+++ pkg/sentry/kernel/rwstring_hook.go
@@ -1,17 +1,16 @@
-package callbacks
+package kernel
 
 import (
 	"gvisor.dev/gvisor/pkg/hostarch"
-	"gvisor.dev/gvisor/pkg/sentry/kernel"
 )
 
-func ReadStringHook(t *kernel.Task) func(addr uintptr, len int) (string, error) {
+func ReadStringHook(t *Task) func(addr uintptr, len int) (string, error) {
 	return func(addr uintptr, length int) (string, error) {
 		return t.CopyInString(hostarch.Addr(addr), length)
 	}
 }
 
-func WriteStringHook(t *kernel.Task) func(addr uintptr, str string) (int, error) {
+func WriteStringHook(t *Task) func(addr uintptr, str string) (int, error) {
 	return func(addr uintptr, str string) (int, error) {
 		bytes := []byte(str)
 		return t.CopyOutBytes(hostarch.Addr(addr), bytes)
