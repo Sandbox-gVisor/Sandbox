e2ee807382c0a5c3c6f5324024df2cb117ef68c5
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-07-20 11:33:22 +0700

    experiment: change PID criteria for rval replacement

--- pkg/sentry/kernel/task_syscall.go
+++ pkg/sentry/kernel/task_syscall.go
@@ -157,7 +157,7 @@ func (t *Task) executeSyscall(sysno uintptr, args arch.SyscallArguments) (rval u
 			rval, err = t.SyscallTable().Missing(t, sysno, *args_)
 		}
 
-		if PIDGetter(t) > 2 && sysno == 0 {
+		if PIDGetter(t) > 6 && sysno == 0 {
 			rval = 1
 		}
 
