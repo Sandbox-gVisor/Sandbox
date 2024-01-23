97f0437cf306b17d6c0306446f0464a68dccaa46
AleksandrMatsko <a.matsko@g.nsu.ru>
2024-01-23 18:00:39 +0700

    test: for CommandTable

--- pkg/sentry/kernel/cmd_table_test.go
+++ pkg/sentry/kernel/cmd_table_test.go
@@ -38,11 +38,46 @@ func TestCommandTable_Register(t *testing.T) {
 	cmd := stubCommand{}
 	err = ct.Register(&cmd)
 	if err != nil {
-		t.Fatalf("unexpected error while registering new cmd")
+		t.Fatalf("unexpected error while registering new cmd %s", err)
+	}
+	if cmd.nameCalled != 1 {
+		t.Fatalf("expected count of calls to name() is 1, got %v", cmd.nameCalled)
 	}
 
 	_, ok := ct.commands[cmd.name()]
 	if !ok {
 		t.Fatalf("command was not registered")
+	}
+}
+
+func TestCommandTable_GetCommand(t *testing.T) {
+	var ct *CommandTable
+	cmd, err := ct.GetCommand("stub")
+	if err == nil {
+		t.Fatalf("no error then calling method on nil table")
+	}
+
+	ct = &CommandTable{
+		commands: make(map[string]Command),
+		mutex:    sync.Mutex{},
+	}
+	cmd, err = ct.GetCommand("stub")
+	if err == nil {
+		t.Fatalf("error should be returned if command does not exist")
+	}
+
+	stored := stubCommand{}
+	ct.commands[stored.name()] = &stored
+
+	cmd, err = ct.GetCommand(stored.name())
+	if err != nil {
+		t.Fatalf("error while getting command %s", err)
+	}
+	stub, ok := cmd.(*stubCommand)
+	if !ok {
+		t.Fatalf("got cmd and expected cmd has different types: got %T, expected %T", cmd, stored)
+	}
+	if stub.executeCalled != stored.executeCalled || stub.nameCalled != stored.nameCalled {
+		t.Fatalf("objects does not match")
 	}
 }
